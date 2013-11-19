b6890f6790d838265dd836deea9ef9aa5f3779de
varthandi <varthandi@users.noreply.github.com>
2013-11-19 00:45:35 -0800

    Update StateMachine.js
    this.everything = uhhhgg;

--- StateMachine.js
+++ StateMachine.js
@@ -3,55 +3,62 @@
 // State, Transition, Finite State Machine
 
 function State(action, entryAction, exitAction) {
-	this.action = action;
-	this.entryAction = action;
-	this.exitAction = action;
+	this.setAction = function(action) {this.action = action;};
+	this.getAction = function() {return this.action;};
 	
-	this.setTransitions = function(trans) {this.transitions=trans};
-	this.getAction = function() {return this.action;}
-	this.getEntryAction = function() {return this.entryAction;}
-	this.getExitAction = function() {return this.exitAction;}
-	this.getTransitions = function() {return this.transitions;}
+	this.setEntryAction = function(action) {this.entryAction = action;};
+	this.getEntryAction = function() {return this.entryAction;};
+	
+	this.setExitAction = function(action) {this.exitAction = action;};
+	this.getExitAction = function() {return this.exitAction;};
+	
+	this.setTransitions = function(trans) {this.transitions=trans;};
+	this.getTransitions = function() {return this.transitions;};
 }
 
-function Transition(targetState, action, condition) {
-	this.targetState = targetState;
-	this.action = action;
-	this.condition = condition;
+function Transition() {
+	this.setTargetState = function(state) {this.targetState = state;};
+	this.getTargetState = function() {return this.targetState;};
 	
-	this.getTargetState = function() {return this.targetState;}
-	this.getAction = function() {return this.action;}
-	this.isTriggered = function() {return condition;}
+	this.setAction = function(action) {this.action = action;};
+	this.getAction = function() {return this.action;};
+	
+	this.setCondition = function(condition) {this.condition = condition;};
+	this.isTriggered = function() {return this.condition.test();};
 }
 
-function StateMachine(currentState) {
-	this.currentState = currentState;
-	
+function Condition() {
+	this.test = function() {return false;};
+}
+
+function StateMachine() {	
 	this.update = function() {
-		var actions = new Array();
-		var transition = null;
-		var targetState;
+		this.actions = new Array();
+		this.transition = null;
+		this.targetState;
 		
-		for(var trans in currentState.getTransitions()) {
-			if (trans.isTriggered()) {
-				this.transition = trans;
+		var trans = this.currentState.getTransitions();
+		for(var i = 0; i < trans.length; i++) {
+			if (trans[i].isTriggered()) {
+				this.transition = trans[i];
 				break;
 			}
 		}
 		
-		if (transition != null) {
-			var targetState = transition.getTargetState();
-			if (currentState.getExitAction() != null) actions.push(currentState.getExitAction());
-			if (transition.getAction() != null) actions.push(transition.getAction());
-			if (targetState.getEntryAction() != null) actions.push(targetState.getEntryAction());
-			this.currentState = targetState;
-			return actions;
+		if (this.transition != null) {
+			this.targetState = this.transition.getTargetState();
+			if (this.currentState.getExitAction() != null) this.actions.push(this.currentState.getExitAction());
+			if (this.transition.getAction() != null) this.actions.push(this.transition.getAction());
+			if (this.targetState.getEntryAction() != null) this.actions.push(this.targetState.getEntryAction());
+			this.currentState = this.targetState;
+			return this.actions;
 		}
 		else {
-			actions.push(currentState.getAction());
-			return actions;
+			this.actions.push(this.currentState.getAction());
+			return this.actions;
 		}
-	}
+	};
 	
-	this.getCurrentState = function() {return currentState;};
+	this.setCurrentState = function(state) {this.currentState = state;};
+	this.getCurrentState = function() {return this.currentState;};
 }
